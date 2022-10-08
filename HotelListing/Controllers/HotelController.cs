c2f178db9c7881a2460dd6b1bea1cc29dbc4a4be
marindimitrov12 <63950527+marindimitrov12@users.noreply.github.com>
2022-10-08 18:35:46 +0300

    Add create hotel action

--- HotelListing/Controllers/HotelController.cs
+++ HotelListing/Controllers/HotelController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HotelListing.Data;
 using HotelListing.IRepository;
 using HotelListing.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -39,7 +40,7 @@ namespace HotelListing.Controllers
                 return StatusCode(500, "Internal Servaer Error.Please Try Again Later.");
             }
         }
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}",Name ="GetHotel")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Authorize]
@@ -55,6 +56,31 @@ namespace HotelListing.Controllers
             {
                 _logger.LogError(ex, $"Something Went Wrong in the {nameof(GetHotel)}");
                 return StatusCode(500, "Internal Servaer Error.Please Try Again Later.");
+            }
+        }
+        [Authorize(Roles ="Administrator")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateHotel([FromBody]CreateHotelDTO hotelDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError($"Invalid POST attempt in {nameof(CreateHotel)}");
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var hotel = _mapper.Map<Hotel>(hotelDTO);
+                await _unitOfWork.Hotels.Insert(hotel);
+                await _unitOfWork.Save();
+                return CreatedAtRoute("GetHotel", new { id=hotel.Id},hotel);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,$"Something Went Wrong in the {nameof(CreateHotel)} ");
+                return StatusCode(500,"Internal Server Error.Please Try Again Later");
             }
         }
     }
