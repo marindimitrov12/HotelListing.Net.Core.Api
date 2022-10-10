b52015e1a0424a0abddcfa294303bd91bae6194a
marindimitrov12 <63950527+marindimitrov12@users.noreply.github.com>
2022-10-10 15:33:50 +0300

    add delete to the country controller

--- HotelListing/Controllers/CountryController.cs
+++ HotelListing/Controllers/CountryController.cs
@@ -114,6 +114,37 @@ namespace HotelListing.Controllers
                 return StatusCode(500, "Internal server error.Please try again Later.");
             }
         }
+        [Authorize]
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            if (id < 1)
+            {
+                _logger.LogError($"Invalid Delete attempt in {nameof(DeleteCountry)}");
+                return BadRequest();
+            }
+            try
+            {
+                var country = await _unitOfWork.Hotels.Get(q => q.Id == id);
+                if (country == null)
+                {
+                    _logger.LogError($"Invalid Delete attempt in {nameof(DeleteCountry)}");
+                    return BadRequest("Submitted data is invalid");
+                }
+                await _unitOfWork.Countries.Delete(id);
+                await _unitOfWork.Save();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, $"Something Went Wrong in the {nameof(DeleteCountry)}");
+                return StatusCode(500, "Internal Server Error.Please try again later");
+            }
+        }
     }
 }
 
